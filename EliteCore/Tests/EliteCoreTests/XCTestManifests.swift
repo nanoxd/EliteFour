ff4b874149f5d0550f4a95a6bcba18f0361b12a7
Fernando Paredes <nano@fdp.io>
2019-11-27 16:42:40 -0500

    Move Regex into EliteCore

--- EliteCore/Tests/EliteCoreTests/XCTestManifests.swift
+++ EliteCore/Tests/EliteCoreTests/XCTestManifests.swift
@@ -4,6 +4,7 @@ import XCTest
     public func allTests() -> [XCTestCaseEntry] {
         [
             testCase(EliteCoreTests.allTests),
+            testCase(RegexTests.allTests),
         ]
     }
 #endif
