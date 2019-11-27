6e4bc19980793aa35d85e455c6948dffcdc33bd3
Fernando Paredes <nano@fdp.io>
2019-11-27 13:57:49 -0500

    Add script to initialize years

--- Advent/Tests/AdventTests/AdventTests.swift
+++ Advent/Tests/AdventTests/AdventTests.swift
@@ -2,12 +2,7 @@
 import XCTest
 
 final class AdventTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Advent().text, "Hello, World!")
-    }
+    func testExample() {}
 
     static var allTests = [
         ("testExample", testExample),
