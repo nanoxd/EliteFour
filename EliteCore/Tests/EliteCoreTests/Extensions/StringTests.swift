ac8f1c9175a04a83027865f3f1d87f84ac1c97ed
Fernando Paredes <nano@fdp.io>
2019-11-27 18:51:43 -0500

    Add String.intersection

--- EliteCore/Tests/EliteCoreTests/Extensions/StringTests.swift
+++ EliteCore/Tests/EliteCoreTests/Extensions/StringTests.swift
@@ -9,7 +9,15 @@ final class StringTests: XCTestCase {
         XCTAssertEqual(firstID.distance(to: secondID), 2)
     }
 
+    func test_intersection() {
+        let firstID = "abcdef"
+        let secondID = "abcdfs"
+
+        XCTAssertEqual(firstID.intersection(with: secondID), "abcd")
+    }
+
     static var allTests = [
         ("test_distance_betweenTwoIDs", test_distance_betweenTwoIDs),
+        ("test_intersection", test_intersection),
     ]
 }
