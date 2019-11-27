13ceb47fff9300b73b6ec2bba25d1b7da10a61a3
Fernando Paredes <nano@fdp.io>
2019-11-27 17:32:47 -0500

    Add Nano to EliteCore

--- EliteCore/Package.swift
+++ EliteCore/Package.swift
@@ -5,13 +5,15 @@ import PackageDescription
 
 let package = Package(
     name: "EliteCore",
+    platforms: [.macOS(.v10_14)],
     products: [
         .library(name: "EliteCore", targets: ["EliteCore"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/nanoxd/Nano", "0.0.2"..."1.0.0")
     ],
     targets: [
-        .target(name: "EliteCore", dependencies: []),
+        .target(name: "EliteCore", dependencies: ["Nano"]),
         .testTarget(name: "EliteCoreTests", dependencies: ["EliteCore"]),
     ]
 )
