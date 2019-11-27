f0e2ba4ec16686a2e70f8d967dc4ce3e0fda7b89
Fernando Paredes <nano@fdp.io>
2019-11-27 18:19:47 -0500

    Add Sequence.histogram

--- EliteCore/Tests/EliteCoreTests/Extensions/SequenceTests.swift
+++ EliteCore/Tests/EliteCoreTests/Extensions/SequenceTests.swift
@@ -12,7 +12,21 @@ final class SequenceTests: XCTestCase {
         XCTAssertEqual(cycledNumbers.next(), 2)
     }
 
+    func test_histogram_displaysOccurrance() {
+        let characters = ["Ash", "Misty", "Brock"]
+        let letterOccurance = characters
+            .flatMap { Array($0) }
+            .histogram()
+
+        print(letterOccurance)
+
+        XCTAssertNil(letterOccurance["d"])
+        XCTAssertEqual(letterOccurance["M"], 1)
+        XCTAssertEqual(letterOccurance["s"], 2)
+    }
+
     static var allTests = [
         ("test_cycled_cyclesThroughSequence", test_cycled_cyclesThroughSequence),
+        ("test_histogram_displaysOccurrance", test_histogram_displaysOccurrance),
     ]
 }
