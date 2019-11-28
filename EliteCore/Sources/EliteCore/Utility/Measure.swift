c3b21a30a60c311a3052b7cb880824071a3c67fe
Fernando Paredes <nano@fdp.io>
2019-11-27 20:00:32 -0500

    [EliteCore] Publish `measure`

--- EliteCore/Sources/EliteCore/Utility/Measure.swift
+++ EliteCore/Sources/EliteCore/Utility/Measure.swift
@@ -7,7 +7,7 @@ import Cocoa
 ///   - closures: Closure that is timed
 /// - Returns: The output of the block
 @discardableResult
-func measure<A>(name: String = "", _ closure: () -> A) -> A {
+public func measure<A>(name: String = "", _ closure: () -> A) -> A {
     let startTime = CACurrentMediaTime()
     let result = closure()
     let timeElapsed = CACurrentMediaTime() - startTime
