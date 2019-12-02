1ccf2eab85c6705d818cb1135739ce485967b1cb
Fernando Paredes <nano@fdp.io>
2019-12-01 20:11:54 -0500

    [Advent 2015-1] Solve Part 1

--- Advent/Sources/AOC2015/Day 1/2015-Day1.swift
+++ Advent/Sources/AOC2015/Day 1/2015-Day1.swift
@@ -1,9 +1,23 @@
 final class Day1: Day {
     override func part1() -> String {
-        ""
+        String(determineNumberOfFloors(input.raw))
     }
 
     override func part2() -> String {
         ""
+    }
+
+    func shouldGoUp(_ str: String) -> Bool {
+        str == "("
+    }
+
+    func determineNumberOfFloors(_ characters: String) -> Int {
+        Array(characters).reduce(into: 0) { floors, character in
+            if shouldGoUp(String(character)) {
+                floors += 1
+            } else {
+                floors -= 1
+            }
+        }
     }
 }
