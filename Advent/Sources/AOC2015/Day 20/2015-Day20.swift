18036f5dede4e7252d22aeab2c695431283b6f39
Fernando Paredes <nano@fdp.io>
2019-12-01 20:04:52 -0500

    [Advent] Add 2015 (#9)

---
+++ Advent/Sources/AOC2015/Day 20/2015-Day20.swift
@@ -0,0 +1,9 @@
+final class Day20: Day {
+    override func part1() -> String {
+        ""
+    }
+
+    override func part2() -> String {
+        ""
+    }
+}
--- Advent/Sources/Advent/Advent.swift
+++ Advent/Sources/Advent/Advent.swift
@@ -1,3 +1,4 @@
 import Foundation
 
+@_exported import AOC2015
 @_exported import AOC2019
