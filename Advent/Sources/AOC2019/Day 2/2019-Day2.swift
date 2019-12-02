2ded5a1db9bc2c5b651c2250c7799b0c67de5c38
Fernando Paredes <nano@fdp.io>
2019-12-02 11:23:19 -0500

    [Advent 2019-2] Refactor Program processing
    * Improves performance by 40x by avoiding creating intermediate chunks and using indexes plus offsets

--- Advent/Sources/AOC2019/Day 2/2019-Day2.swift
+++ Advent/Sources/AOC2019/Day 2/2019-Day2.swift
@@ -36,46 +36,7 @@ final class Day2: Day {
     }
 
     func process(program: [Int]) -> [Int] {
-        var stream = program
-        var currentIndex = 0
-        var chunks: [[Int]] = []
-        var shouldHalt = false
-
-        while !shouldHalt {
-            chunks = stream.chunked(into: 4)
-            let currentSlice = chunks[currentIndex]
-            currentIndex += 1
-
-            guard let opcode = currentSlice.first.flatMap({ OpcodeIndicator(rawValue: $0) }) else {
-                continue
-            }
-
-            switch opcode {
-            case .add:
-                let index1 = currentSlice[1]
-                let index2 = currentSlice[2]
-                let registerIndex = currentSlice[3]
-
-                let value = stream[index1] + stream[index2]
-                stream[registerIndex] = value
-            case .multiply:
-                let index1 = currentSlice[1]
-                let index2 = currentSlice[2]
-                let registerIndex = currentSlice[3]
-
-                let value = stream[index1] * stream[index2]
-                stream[registerIndex] = value
-            case .halt:
-                shouldHalt = true
-            }
-        }
-
-        return stream
+        let intcode = IntCode(memory: program, supportedOperations: [.add, .multiply, .halt])
+        return intcode.run()
     }
-}
-
-enum OpcodeIndicator: Int {
-    case add = 1
-    case multiply = 2
-    case halt = 99
 }
