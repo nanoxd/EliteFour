43e771d73f9ea4bcd4bbcd7bb0364b38a0034e10
Fernando Paredes <nano@fdp.io>
2019-12-03 07:13:23 -0500

    [Advent 2019-3] Solve Part 1 Examples

--- Advent/Sources/AOC2019/Day 3/2019-Day3.swift
+++ Advent/Sources/AOC2019/Day 3/2019-Day3.swift
@@ -1,9 +1,109 @@
 final class Day3: Day {
+    let regex = Regex(pattern: #"([RDLU])(\d+)"#)
+
+    lazy var wires: [Wire] = {
+        input.lines.map { parseLine($0.raw) }
+    }()
+
     override func part1() -> String {
         ""
     }
 
     override func part2() -> String {
         ""
+    }
+
+    func parseLine(_ string: String) -> Wire {
+        let matches = regex.matches(in: string)
+
+        let segments = matches.compactMap { match -> Wire.Segment? in
+            guard
+                let directionString = match[1],
+                let direction = Wire.Direction(rawValue: directionString),
+                let length = match.int(2) else {
+                return nil
+            }
+
+            return Wire.Segment(direction: direction, length: length)
+        }
+
+        return Wire(segments: segments)
+    }
+}
+
+struct Wire {
+    struct Segment {
+        let direction: Direction
+        let length: Int
+
+        var directionalLength: CGFloat {
+            switch direction {
+            case .left, .down:
+                return -CGFloat(length)
+            case .right, .up:
+                return CGFloat(length)
+            }
+        }
+
+        var transform: CGAffineTransform {
+            switch direction {
+            case .right, .left:
+                return CGAffineTransform(translationX: directionalLength, y: 0)
+            case .up, .down:
+                return CGAffineTransform(translationX: 0, y: directionalLength)
+            }
+        }
+    }
+
+    enum Direction: Equatable, CaseIterable {
+        case right
+        case left
+        case down
+        case up
+
+        init?(rawValue: String) {
+            switch rawValue {
+            case "R":
+                self = .right
+            case "L":
+                self = .left
+            case "D":
+                self = .down
+            case "U":
+                self = .up
+            default: return nil
+            }
+        }
+    }
+
+    var segments: [Segment]
+
+    func breadcrumbs() -> [CGPoint] {
+        segments
+            .flatMap { segment in
+                (0..<segment.length).replay(segment)
+            }
+            .reduce(into: [CGPoint.zero]) { points, segment in
+                points.append(
+                    points[points.endIndex - 1].byMoving(direction: segment.direction)
+                )
+            }
+    }
+}
+
+extension Sequence {
+    func replay<T>(_ replayer: @autoclosure () -> T) -> [T] {
+        map { _ in replayer() }
+    }
+}
+
+extension CGPoint {
+    func byMoving(direction: Wire.Direction) -> CGPoint {
+        switch direction {
+        case .right: return applying(.init(translationX: 1, y: 0))
+        case .left: return applying(.init(translationX: -1, y: 0))
+        case .up: return applying(.init(translationX: 0, y: 1))
+        case .down: return applying(.init(translationX: 0, y: -1))
+        }
     }
 }
--- Advent/Tests/AdventTests/Test2019.swift
+++ Advent/Tests/AdventTests/Test2019.swift
@@ -72,6 +72,61 @@ class Test2019: XCTestCase {
         XCTAssertEqual(p2, "")
     }
 
+    func test_day3_regex() {
+        let day = Day3()
+        let input = """
+        R75,D30,R83,U83,L12,D49,R71,U7,L72
+        """
+
+        let matches = day.regex.matches(in: input)
+
+        XCTAssertEqual(matches.count, 9)
+
+        let firstMatch = matches.first
+        XCTAssertEqual(firstMatch?[1], "R")
+        XCTAssertEqual(firstMatch?.int(2), 75)
+
+        let middleMatch = matches[4]
+        XCTAssertEqual(middleMatch[1], "L")
+        XCTAssertEqual(middleMatch.int(2), 12)
+    }
+
+    func test_day3Part1_example1() {
+        let day = Day3()
+
+        let testInput = ["R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83"]
+            .map(day.parseLine(_:))
+
+        let breadcrumbs1 = testInput.first!.breadcrumbs()
+        let breadcrumbs2 = testInput.last!.breadcrumbs()
+
+        let distance = breadcrumbs2
+            .dropFirst()
+            .filter { breadcrumbs1.contains($0) }
+            .map { $0.manhattanDistance }
+            .min()
+
+        XCTAssertEqual(distance, 159)
+    }
+
+    func test_day3Part1_example2() {
+          let day = Day3()
+
+          let testInput = ["R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"]
+              .map(day.parseLine(_:))
+
+          let breadcrumbs1 = testInput.first!.breadcrumbs()
+          let breadcrumbs2 = testInput.last!.breadcrumbs()
+
+          let distance = breadcrumbs2
+              .dropFirst()
+              .filter { breadcrumbs1.contains($0) }
+              .map { $0.manhattanDistance }
+              .min()
+
+          XCTAssertEqual(distance, 135)
+      }
+
     func testDay4() {
         let d = Day4()
         let (p1, p2) = d.run()
