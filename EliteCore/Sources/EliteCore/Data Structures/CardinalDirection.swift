9c55f6cd266903ac2947a2926b6bb2d6d069e5d7
Fernando Paredes <nano@fdp.io>
2019-12-03 22:09:39 -0500

    [EliteCore] Add Cardinal Direction

---
+++ EliteCore/Sources/EliteCore/Data Structures/CardinalDirection.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+/// Corresponds to Cardinal directions
+public enum CardinalDirection: Equatable, CaseIterable {
+    case north
+    case south
+    case east
+    case west
+}
