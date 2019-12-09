b8501950b1d7c57ad5ca82dee46ef1c8333e964f
Fernando Paredes <nano@fdp.io>
2019-12-09 06:39:12 -0500

    [Advent 2019] Day 9 (#22)
    * [Advent 2019-9] Solve Part 1 Examples
    
    * [Advent 2019-9] Solve Part 1
    
    * [Advent 2019-9] Solve Part 2

--- Advent/Sources/AOC2019/Day 9/2019-Day9.swift
+++ Advent/Sources/AOC2019/Day 9/2019-Day9.swift
@@ -1,9 +1,26 @@
+/// Sensor Boost
 final class Day9: Day {
+    lazy var programValues: [Int] = {
+        input.integers
+    }()
+
+    /// Once your Intcode computer is fully functional, the BOOST
+    /// program should report no malfunctioning opcodes when run
+    /// in test mode; it should only output a single value, the BOOST
+    /// keycode. What BOOST keycode does it produce?
     override func part1() -> String {
-        ""
+        let processedMemory = process(program: programValues, input: [1])
+        return "\(processedMemory.outputs.first ?? 0)"
     }
 
     override func part2() -> String {
-        ""
+        let processedMemory = process(program: programValues, input: [2])
+        return "\(processedMemory.outputs.first ?? 0)"
+    }
+
+    func process(program: [Int], input: [Int]) -> (outputs: [Int], termination: Int) {
+        let computer = IntCode2(memory: program, inputs: input)
+
+        return computer.executeUntilTermination()
     }
 }
